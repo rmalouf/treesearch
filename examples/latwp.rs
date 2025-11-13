70d4bdf90ba81aaf8385f424d49649c585ad9e37
Rob Malouf <rmalouf@sdsu.edu>
2025-11-12 18:42:04 -0800

    Update latwp example to demonstrate NOUN-of-NOUN pattern matching
    Change the example query from adjective-noun modification to prepositional
    phrases with "of", showing a more complex multi-edge pattern with unlabeled
    dependency relations.

--- examples/latwp.rs
+++ examples/latwp.rs
@@ -9,9 +9,11 @@ fn main() {
         CoNLLUReader::from_file("./examples/lw970831.conll".as_ref()).expect("Can't open file");
 
     let query = r#"
-        Noun [pos="NOUN"];
-        Adj [pos="ADJ"];
-        Noun -[amod]-> Adj;
+        N1 [pos="NOUN"];
+        Of [form="of"];
+        N2 [pos="NOUN"];
+        N1 -> Of;
+        Of -> N2;
     "#;
 
     for tree in reader {
@@ -21,8 +23,9 @@ fn main() {
             // result is a Vec<WordId> ordered by variable declaration
             // Variable 0 = Noun, Variable 1 = Adj
             let noun = tree.get_word(result[0]).unwrap();
-            let adj = tree.get_word(result[1]).unwrap();
-            println!("{} -> {}", noun.form, adj.form)
+            let adj = tree.get_word(result[2]).unwrap();
+            println!("{} of {}", noun.form, adj.form)
         }
+        println!();
     }
 }
