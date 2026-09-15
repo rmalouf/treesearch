908fa89c9864485d5e1819d0e4999bc4ef35df32
malouf <rmalouf@sdsu.edu>
2026-09-15 11:07:12 -0700

    Update help_to example queries and output columns

--- examples/help.py
+++ examples/help.py
@@ -1,5 +1,5 @@
 ## collect data for help_to paper
-##   20-Mar-2026
+##   07-Jul-2026
 
 import treesearch
 import polars as pl
@@ -7,17 +7,32 @@ import time
 
 PATH = "/Volumes/Corpora/CCOHA/conllu/*.conllu.gz"
 
-# VERB_QUERY = """
-# MATCH {
-#     Head [upos="VERB"];
-#     XComp [upos="VERB" & feats.VerbForm="Inf"];
-#     Head -/[xc]comp/-> XComp;
-# }
-# """
-
 VERB_QUERY = """
 MATCH {
     Head [upos="VERB"];
+    XComp [upos="VERB" & feats.VerbForm="Inf"];
+    Head -/[cx]comp/-> XComp;
+    Head !-[auxpass]-> _;
+    _ !-[conj]-> Head;
+    Head !-[conj]-> _;
+    _ !-[conj]-> XComp;
+    XComp !-[conj]-> _;
+    Head << XComp;
+}
+EXCEPT {
+    But [lemma="but"];
+    Head << But;
+    But << XComp;
+}
+OPTIONAL {
+    HeadTo [upos="PART" & lemma="to"];
+    Head -> HeadTo;
+    HeadTo << Head;
+}
+OPTIONAL {
+    XCompTo [upos="PART" & lemma="to"];
+    XComp -> XCompTo;
+    XCompTo << XComp;
 }
 """
 
@@ -27,29 +42,45 @@ def verbs():
 
     treebank = treesearch.load(PATH)
     for tree, match in treebank.search(pattern, ordered=False):
-        head = tree[match["Head"]].lemma
-        doc_id = tree.metadata["doc_id"]
-        data.append({"verb": head, "year": doc_id})
-
-        if head == "clearp":
-            print(doc_id)
-            print(tree.sentence_text)
-            print(tree.metadata["sent_id"])
+        head = tree[match["Head"]]
+        xcomp = tree[match["XComp"]]
+        data.append(
+            {
+                "head_form": head.form.lower(),
+                "head_lemma": head.lemma.lower(),
+                "transitive": check_dep(tree, head, "dobj") or check_dep(tree, xcomp, "nsubj"),
+                "head_to": "HeadTo" in match,
+                "xcomp_lemma": xcomp.lemma,
+                "bare_inf": "XCompTo" not in match,
+                "xcomp_transitive": check_dep(tree, xcomp, "dobj")
+                or check_dep(tree, xcomp, "ccomp"),
+                "distance": int(xcomp.id - head.id),
+                "doc_id": tree.metadata["doc_id"],
+                "sent_id": tree.metadata["sent_id"],
+                "text": tree.sentence_text,
+            }
+        )
 
     df = (
         pl.DataFrame(data)
-        .with_columns(pl.col("year").str.extract(r"_([0-9]+)", group_index=1))
+        .with_columns(pl.col("doc_id").str.extract(r"_([0-9]+)", group_index=1).alias("year"))
         .sort("year")
     )
     df.write_parquet("verbs.parquet")
 
 
-def check_dep(tree, node, deprel, tag=None):
+
+
+def get_dep(tree, node, deprel, tag=None):
     deps = node.children_by_deprel(deprel)
     for dep in deps:
         if tag is None or dep.xpos == tag:
-            return True
-    return False
+            return dep
+    return None
+
+def check_dep(tree, node, deprel, tag=None):
+    return get_dep(tree, node, deprel, tag) is not None
+
 
 
 HELP_QUERY = """
@@ -66,24 +97,22 @@ MATCH {
 }
 EXCEPT {
     But [lemma="but"];
-    XComp -[cc]-> But;
-    Head !-[neg]-> _;
+    Head << But;
+    But << XComp;
+}
+EXCEPT {
+    ItSubj [lemma="it"];
+    Head -[nsubj]-> ItSubj;
 }
 OPTIONAL {
-    HeadTo [lemma="to"];
-    Head -[aux]-> HeadTo;
+    HeadTo [upos="PART" & lemma="to"];
+    Head -> HeadTo;
+    HeadTo << Head;
 }
 OPTIONAL {
-    XCompTo [lemma="to"];
-    XComp -[aux]-> XCompTo;
-}
-OPTIONAL {
-    XCompNeg [lemma="not"];
-    XComp -[neg]-> XCompNeg;
-}
-OPTIONAL {
-    HeadNeg [lemma="not"];
-    Head -[neg]-> HeadNeg;
+    XCompTo [upos="PART" & lemma="to"];
+    XComp -> XCompTo;
+    XCompTo << XComp;
 }
 """
 
@@ -96,19 +125,18 @@ def helps():
     for tree, match in treebank.search(pattern, ordered=False):
         head = tree[match["Head"]]
         xcomp = tree[match["XComp"]]
-        head_neg = "HeadNeg" in match
-        xcomp_neg = "XCompNeg" in match
-
+        subj = get_dep(tree, head, "nsubj") 
+        it_subj = subj is not None and subj.lemma == "it"
+        #print(it_subj)
         data.append(
             {
                 "head_form": head.form.lower(),
                 "transitive": check_dep(tree, head, "dobj") or check_dep(tree, xcomp, "nsubj"),
                 "head_to": "HeadTo" in match,
-                "head_aux": check_dep(tree, head, "aux"),
-                "head_neg": head_neg,
-                "xcomp_neg": xcomp_neg,
+                "it_subj": it_subj,
                 "xcomp_lemma": xcomp.lemma,
                 "bare_inf": "XCompTo" not in match,
+                "helpee_pron": check_dep(tree, head, "dobj", "PRP") or check_dep(tree, xcomp, "nsubj", "PRP"),
                 "xcomp_transitive": check_dep(tree, xcomp, "dobj")
                 or check_dep(tree, xcomp, "ccomp"),
                 "distance": int(xcomp.id - head.id),
@@ -117,7 +145,11 @@ def helps():
                 "text": tree.sentence_text,
             }
         )
-    df = pl.DataFrame(data)
+    df = (
+        pl.DataFrame(data)
+        .with_columns(pl.col("doc_id").str.extract(r"_([0-9]+)", group_index=1).alias("year"))
+        .sort("year")
+    )
     df.write_parquet("help.parquet")
 
 
@@ -193,9 +225,9 @@ def dares():
 
 
 if __name__ == "__main__":
-    print("verbs")
-    verbs()
+#    print("verbs")
+#    verbs()
     print("helps")
     helps()
-    print("dares")
-    dares()
+#    print("dares")
+#    dares()
